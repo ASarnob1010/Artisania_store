90c37f0c842dbd8cff5e1b81114c95721a3cc176
ASarnob1010 <153275698+ASarnob1010@users.noreply.github.com>
2024-09-15 08:04:49 +0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import 'remixicon/fonts/remixicon.css'
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom'
+import Products from './components/Admin/Product'
+import Orders from './components/Admin/Orders'
+import Dashboard from './components/Admin/Dashboard'
+import NotFound from './components/NotFound'
+import Payments from './components/Admin/Payments'
+import Settings from './components/Admin/Settings'
+import Customers from './components/Admin/Customers'
+
+const App = ()=>{
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/admin">
+          <Route path="products" element={<Products />} />
+          <Route path="customers" element={<Customers />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="payments" element={<Payments />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
